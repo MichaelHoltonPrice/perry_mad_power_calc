b9120dada43e0cb0404b9cb874efcba789fcf7b8
MICHAEL HOLTON PRICE <mhp12@psu.edu>
2016-03-29 13:00:56 -0400

    Began object oriented structure for file parsing

--- code/power_calc_sandbox.py
+++ code/power_calc_sandbox.py
@@ -1,14 +1,15 @@
 import stru_to_arp
 import os
 
-gitDir = os.path.join('C:'+os.sep,'Users','mhp12','git')
+#gitDir = os.path.join('C:'+os.sep,'Users','mhp12','git')
+gitDir = os.path.join('C:'+os.sep,'Users','Michael','git')
 projDir = os.path.join(gitDir,'perry_mad_power_calc')
 
 inputFile = os.path.join(projDir,'data','original','PembertonEtAl2013data-G3','pembertonEtAl2013.MS5795.stru')
 outputFile = os.path.join(projDir,'data','original','abc_toolbox','pembertonEtAl2013.MS5795.arp')
 
-
-data = stru_to_arp.stru_to_arp(inputFile,outputFile,title="PembertonEtAl2013data-G3 for Perry Lab simulation")
+stru = stru_to_arp.Stru(inputFile)
+#data = stru_to_arp.stru_to_arp(inputFile,outputFile,title="PembertonEtAl2013data-G3 for Perry Lab simulation")
 
 
 #print(data[len(data)-1])
--- code/stru_to_arp.py
+++ code/stru_to_arp.py
@@ -1,39 +1,81 @@
 import os
 
-def stru_to_arp(inputFile,outputFile,title=""):
-  # The following pdf contains a description of the target .arp file type
-  # http://cmpg.unibe.ch/software/arlequin35/man/Arlequin35.pdf
-  fin = open(inputFile, 'r')
-  fout = open(outputFile, 'w+')
-  
-  fout.write("[Profile]\n")
-  fout.write("\tTitle=\"" + title + "\"\n")
-  fout.write("\tNbSamples=1\n")
-  fout.write("\n")
-  fout.write("\tGenotypicData=1\n")
-  fout.write("\tGameticPhase=0\n")
-  fout.write("\tRecessiveData=0\n")
-  fout.write("\tDataType=MICROSAT\n")
-  fout.write("\tLocusSeparator=WHITESPACE\n")
-  fout.write("\tMissingData=\'?\'\n")
-  fout.write("\n")
-  fout.write("[Data]\n")
-  fout.write("\t[[Samples]]\n")
-  fout.write("\n")
-  fout.write("\n")
-  fout.write("\t\tSampleName=\"Sample 1\"\n")
-  fout.write("\t\tSampleSize=25\n")
-  fout.write("\t\tSampleData= {\n")
-  #numLines = sum(1 for line in open(inputFile))
-  data = []
-  for n,line in enumerate(fin):
-    #print(line)
-    if(n>1):
-      data.append(line)
-      tokens = line.split(" ")
-      descriptors = tokens[0:5]
-      loci = tokens[5:len(tokens)]
+class Stru:
+    # A class to parse and store the data in a genetic Stru file. This will
+    # likely not work for all types of Stru files, but it does work with the
+    # Pemberton et al. 2013 Stru file.
+    def __init__(self,filePath):
+        self.sampleDict = dict()
+        fin = open(filePath, 'r')
+        for n,line in enumerate(fin):
+            #print(n)
+            if(n==1):
+                self.header = line.split(" ")
+            else:
+                tokens = line.split(" ")
+		# Descriptors consists of (from readme file):
+                # (1) Individual code number assigned by the source publication
+                # (2) Population code number assigned by the source publication
+                # (3) Population name
+                # (4) Location
+                # (5) Pre-defined geographic region affiliation
+                # The individual and population code together comprise a unique
+		# key for the sample dictionary
+                sampleId = (tokens[0],tokens[1])
+                #print(sampleId)
+                loci = tokens[5:len(tokens)]
+                self.sampleDict[sampleId] = (tokens[2],tokens[3],tokens[4],loci)
+        fin.close()
+        
 
-  fin.close()
-  fout.close()
-  return(data)
+#class Pemberton:
+#    # A class to parse and store the tandem repeat and associated data from
+#    # Pemberton et al. 2013 -- Population structure in a comprehensive data
+#    # set on human microsatellite variation. The data is located here:
+#    #
+#    # https://web.stanford.edu/group/rosenberglab/diversity.html
+#    def __init__(self,dataDir):
+#        # Assume the data is all stored in dataDir with the original filenames.
+#        os.path.join(dataDir,"pembertonEtAl2013.MS5879.stru")
+#
+#    @staticmethod
+#    def parse_stru_file(filePath):
+#        
+
+def stru_to_arp(inputFile,outputFile,title=""):
+    # The following pdf contains a description of the target .arp file type
+    # http://cmpg.unibe.ch/software/arlequin35/man/Arlequin35.pdf
+    fin = open(inputFile, 'r')
+    fout = open(outputFile, 'w+')
+    
+    fout.write("[Profile]\n")
+    fout.write("\tTitle=\"" + title + "\"\n")
+    fout.write("\tNbSamples=1\n")
+    fout.write("\n")
+    fout.write("\tGenotypicData=1\n")
+    fout.write("\tGameticPhase=0\n")
+    fout.write("\tRecessiveData=0\n")
+    fout.write("\tDataType=MICROSAT\n")
+    fout.write("\tLocusSeparator=WHITESPACE\n")
+    fout.write("\tMissingData=\'?\'\n")
+    fout.write("\n")
+    fout.write("[Data]\n")
+    fout.write("\t[[Samples]]\n")
+    fout.write("\n")
+    fout.write("\n")
+    fout.write("\t\tSampleName=\"Sample 1\"\n")
+    fout.write("\t\tSampleSize=25\n")
+    fout.write("\t\tSampleData= {\n")
+    #numLines = sum(1 for line in open(inputFile))
+    data = []
+    for n,line in enumerate(fin):
+      #print(line)
+      if(n>1):
+        data.append(line)
+        tokens = line.split(" ")
+        descriptors = tokens[0:5]
+        loci = tokens[5:len(tokens)]
+
+    fin.close()
+    fout.close()
+    return(data)
